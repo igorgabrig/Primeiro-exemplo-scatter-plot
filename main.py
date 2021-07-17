99411a1c39a0f6c3f95247f9643edac06f3bebd9
igorgabrig <igor_gabrig@hotmail.com>
2021-07-17 16:43:24 -0300

    super heros

--- main.py
+++ main.py
@@ -7,17 +7,16 @@ import plotly.express as px;
 import xlrd;
 
 
+
 print("hello");
 
 ##Importando o data set
-url= ('https://github.com/igorgabrig/Primeiro-exemplo-scatter-plot/blob/main/arq/euro2020.csv?raw=true')
+url= ('https://github.com/igorgabrig/Primeiro-exemplo-scatter-plot/blob/main/arq/euro2020.xlsx?raw=true')
 df= pd.read_excel(url)
 df.head()
 
-df_sample = df.sample(n=1000)
-fig  = px.scatter(df_sample, x = 'Goals', hover_name = "Player", opacity=0.5 ,y = 'Total attempts', log_x = True, width = 800)
-fig.update_traces(marker = dict(size = 12, line=dict(width = 2)), selector = dict(mode = 'markers'))
-fig.update_layout(title = 'Logaritmo do PIB per capita X Expectativa de vida')
-fig.update_xaxes(title = 'Log(PIB per capita)')
-fig.update_yaxes(title = 'Expectativa de vida')
-fig.show()
+#print(df)
+
+sns.pairplot(df)
+
+plt.show()
