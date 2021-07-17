bec186dea2c9d1d584e35ea1e663d492f662ebb7
igorgabrig <igor_gabrig@hotmail.com>
2021-07-17 17:42:50 -0300

    arq adicionado

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ url= ('https://github.com/igorgabrig/Primeiro-exemplo-scatter-plot/blob/main/arq
 df= pd.read_excel(url)
 df.head()
 
-print(df['Budget'])
+print(df['Metascore'])
 
 #sns.pairplot(df, height=3.0)
 #plt.show()
